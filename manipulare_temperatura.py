64481bee6212b8f0c15a6dd0ed0d422cc58e4e2c
David Ghergut <david_ghergut@192-168-0-112.rdsnet.ro>
2021-06-26 20:06:14 +0300

    Third Update

--- manipulare_temperatura.py
+++ manipulare_temperatura.py
@@ -27,31 +27,27 @@ def csv_file_reader():
         header = ("Nr.crt", "Temperature_from_Humidity", "Temperature_from_Pressure", "CPU_Temperature", "Humidity", "Pressure")
         csv_writer.writerow(header)
         for line in csv_reader:
-            #print(str(nr) + '.')
             #TEMPERATURE FROM THE HUMIDITY SENSOR
             CalcTemp1 = round((0.0071* (line[0] ** 2) + 0.86 * line[0] - 10.0), 2)
-            #print("temp_humidity: " + str(CalcTemp1))
             #TEMPERATURE FROM THE PRESSURE SENSOR
             #cpuTemp = float(line[2])
             #ambient = float(line[1])
             CalcTemp2 = round(line[1] - ((line[2] - line[1]) / 1.5), 2)
-            #rint("temp_pressure: " + str(CalcTemp2))
-            #print('\n')
             #THE AVERAGE VALUE FOR ALMOST EACH TYPE OF DATA THAT WE COLLECTED
-            avg_temperature_from_Humidity += line[0]
-            avg_temperature_from_Pressure += line[1]
+            avg_temperature_from_Humidity += CalcTemp1
+            avg_temperature_from_Pressure += CalcTemp2
             avg_CPU_Temperature += line[2]
             avg_Humidity += line[3]
             avg_Pressure += line[4]
             #THE MINIMUM AND THE MAXIMUM VALUE FOR ALMOST EACH TYPE OF DATA THAT WE COLLECTED
-            if line[0] < min_temperature_from_Humidity:
-                min_temperature_from_Humidity = line[0]
-            elif line[0] > max_temperature_from_Humidity:
-                max_temperature_from_Humidity = line[0]
-            if line[1] < min_temperature_from_Pressure:
-                min_temperature_from_Pressure = line[1]
-            elif line[1] > max_temperature_from_Pressure:
-                max_temperature_from_Pressure = line[1]
+            if CalcTemp1 < min_temperature_from_Humidity:
+                min_temperature_from_Humidity = CalcTemp1
+            elif CalcTemp1 > max_temperature_from_Humidity:
+                max_temperature_from_Humidity = CalcTemp1
+            if CalcTemp2 < min_temperature_from_Pressure:
+                min_temperature_from_Pressure = CalcTemp2
+            elif CalcTemp2 > max_temperature_from_Pressure:
+                max_temperature_from_Pressure = CalcTemp2
             if line[2] < min_CPU_Temperature:
                 min_CPU_Temperature = line[2]
             elif line[2] > max_CPU_Temperature:
