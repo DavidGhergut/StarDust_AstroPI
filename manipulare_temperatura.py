abae6c41d8acced2cbeb6ce073c587c814d7db8b
David Ghergut <david_ghergut@Davids-MacBook-Pro.local>
2021-06-26 13:50:23 +0300

    Second Update

--- manipulare_temperatura.py
+++ manipulare_temperatura.py
@@ -15,23 +15,28 @@ from pathlib import Path
 def csv_file_reader():
     nr = 1
     dir_path = Path(__file__).parent.resolve()
-    manipulated_file = dir_path/'data.csv'
+    manipulated_file = dir_path/'imported_data.csv'
     avg_temperature_from_Humidity, avg_temperature_from_Pressure, avg_CPU_Temperature, avg_Humidity, avg_Pressure = 0.0, 0.0, 0.0, 0.0, 0.0
     max_temperature_from_Humidity, max_temperature_from_Pressure, max_CPU_Temperature, max_Humidity, max_Pressure = 0.0, 0.0, 0.0, 0.0, 0.0
     min_temperature_from_Humidity, min_temperature_from_Pressure, min_CPU_Temperature, min_Humidity, min_Pressure = 50.0, 50.0, 50.0, 50.0, 10000.0
-    with open(str(manipulated_file), 'r') as data_file:
-        csv_reader = reader(data_file, quoting=QUOTE_NONNUMERIC)
+    data_file = open(str(manipulated_file), 'r')
+    #with open(str(manipulated_file), 'r') as data_file:
+    csv_reader = reader(data_file, quoting=QUOTE_NONNUMERIC)
+    with open('resulted_data.csv', 'w') as obtained_file:
+        csv_writer = writer(obtained_file, delimiter = ',')
+        header = ("Nr.crt", "Temperature_from_Humidity", "Temperature_from_Pressure", "CPU_Temperature", "Humidity", "Pressure")
+        csv_writer.writerow(header)
         for line in csv_reader:
-            print(str(nr) + '.')
+            #print(str(nr) + '.')
             #TEMPERATURE FROM THE HUMIDITY SENSOR
             CalcTemp1 = round((0.0071* (line[0] ** 2) + 0.86 * line[0] - 10.0), 2)
-            print("temp_humidity: " + str(CalcTemp1))
+            #print("temp_humidity: " + str(CalcTemp1))
             #TEMPERATURE FROM THE PRESSURE SENSOR
             #cpuTemp = float(line[2])
             #ambient = float(line[1])
             CalcTemp2 = round(line[1] - ((line[2] - line[1]) / 1.5), 2)
-            print("temp_pressure: " + str(CalcTemp2))
-            print('\n')
+            #rint("temp_pressure: " + str(CalcTemp2))
+            #print('\n')
             #THE AVERAGE VALUE FOR ALMOST EACH TYPE OF DATA THAT WE COLLECTED
             avg_temperature_from_Humidity += line[0]
             avg_temperature_from_Pressure += line[1]
@@ -59,6 +64,8 @@ def csv_file_reader():
                 min_Pressure = line[4]
             elif line[4] > max_Pressure:
                 max_Pressure = line[4]
+            row = (str(nr) + '.', CalcTemp1, CalcTemp2, line[2], line[3], line[4])
+            csv_writer.writerow(row)
             nr += 1
 
                 
